6d5edd5793347b32899e19b4cfb79817975e23e2
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-05-19 17:18:00 +0800

    add PorterDuffXfermode

--- AudioAdjustView/app/src/main/java/com/yfz/main/View/IosColumnAudioView.java
+++ AudioAdjustView/app/src/main/java/com/yfz/main/View/IosColumnAudioView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RectF;
 import android.media.AudioManager;
-import android.net.rtp.AudioStream;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -31,16 +33,30 @@ public class IosColumnAudioView extends View {
     private double mCurrentLoudRate = 0.5;
     //系统最大声音index
     private int mMaxLoud=0;
-    //View背景
-    private Drawable mDrawable_outside = null;
-    //UI背景
-    private Drawable mDrawable_inside = null;
     //记录按压时手指相对于组件view的高度
     private float mDownY;
     //系统audio管理
     private AudioManager mAM;
-    //声音流类型int//默认是音乐类型
+    //画笔
+    private Paint mPaint;
+    //位置
+    private RectF mRectF;
+    /**
+     * 设置声音流类型-默认音乐-iosColumnAudioView_setAudioStreamType
+     */
     private int mAudioStreamInt= AudioManager.STREAM_MUSIC;
+    /**
+     * 设置圆弧度数-xml-iosColumnAudioView_setRadiusXY
+     */
+    private float mRXY=40;
+    /**
+     * 设置当前音量颜色-xml-iosColumnAudioView_setColorLoud
+     */
+    private int mColorLoud = Color.GRAY;
+    /**
+     * 设置组件背景颜色-xml-iosColumnAudioView_setColorBackground
+     */
+    private int mColorBackground = Color.DKGRAY;
 
     public IosColumnAudioView(Context context) {
         super(context);
@@ -50,9 +66,10 @@ public class IosColumnAudioView extends View {
     public IosColumnAudioView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.IosColumnAudioView);
-        mDrawable_outside=typedArray.getDrawable(R.styleable.IosColumnAudioView_IosColumnAudioView_setBackgroundOutSide);
-        mDrawable_inside=typedArray.getDrawable(R.styleable.IosColumnAudioView_IosColumnAudioView_setBackgroundInSide);
-        mAudioStreamInt=typedArray.getInteger(R.styleable.IosColumnAudioView_IosColumnAudioView_setAudioStreamType,mAudioStreamInt);
+        mColorBackground = typedArray.getColor(R.styleable.IosColumnAudioView_iosColumnAudioView_setColorBackground,mColorBackground);
+        mColorLoud = typedArray.getColor(R.styleable.IosColumnAudioView_iosColumnAudioView_setColorLoud,mColorLoud);
+        mAudioStreamInt=typedArray.getInteger(R.styleable.IosColumnAudioView_iosColumnAudioView_setAudioStreamType,mAudioStreamInt);
+        mRXY = typedArray.getDimension(R.styleable.IosColumnAudioView_iosColumnAudioView_setRadiusXY, mRXY);
         initial(context);
     }
 
@@ -70,12 +87,10 @@ public class IosColumnAudioView extends View {
         mAM = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
         mMaxLoud = mAM.getStreamMaxVolume(mAudioStreamInt);
         mCurrentLoudRate = (double) mAM.getStreamVolume(mAudioStreamInt) / mMaxLoud;
-
-        if(null == mDrawable_outside){
-            setBackgroundColor(Color.GRAY);
-        }else {
-            setBackground(mDrawable_outside);
-        }
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mRectF = new RectF();
+        setWillNotDraw(false);
+        setBackgroundColor(Color.TRANSPARENT);
     }
 
     @Override
@@ -115,21 +130,33 @@ public class IosColumnAudioView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        if(null != mDrawable_inside) {
-            mDrawable_inside.setBounds(0,(getHeight()-(int)(getHeight() * mCurrentLoudRate)),getWidth(),getHeight());
-            mDrawable_inside.draw(canvas);
-        }
+        int layerId=canvas.saveLayer(0, 0, canvas.getWidth(), canvas.getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        onDrawLoud(canvas);
+        onDrawRectBackground(canvas,layerId);
+    }
+    private void onDrawLoud(Canvas canvas){
+        mPaint.setColor(mColorLoud);
+        mRectF.left=0;
+        mRectF.top=0;
+        mRectF.right=canvas.getWidth();
+        mRectF.bottom=canvas.getHeight();
+        canvas.drawRoundRect(mRectF,mRXY,mRXY,mPaint);
+    }
+    private void onDrawRectBackground(Canvas canvas,int layerId){
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP));
+        mPaint.setColor(mColorBackground);
+        mRectF.left=0;
+        mRectF.top=(canvas.getHeight()-(int)(canvas.getHeight() * mCurrentLoudRate));
+        mRectF.right=canvas.getWidth();
+        mRectF.bottom=canvas.getHeight();
+        canvas.drawRect(mRectF,mPaint);
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(layerId);
     }
 
     private double px2dip(Context context, double pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (pxValue / scale + 0.5f);
     }
-//
-//    private int getAudioStreamType(String streamType){
-//        switch (streamType){
-//            default:
-//                break;
-//        }
-//    }
+
 }
